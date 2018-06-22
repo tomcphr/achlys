7eef07010f03b117c2748a503e0b8008fdb48250
tomcphr <tomcphr@pm.me>
2018-06-22 09:44:58 +0000

    Added an abstraction for creating objects

--- client/src/Deko/World.ts
+++ client/src/Deko/World.ts
@@ -1,19 +1,18 @@
+import {WorldObject} from "./WorldObject";
+
 export class World
 {
     socket: SocketIOClient.Socket;
     ctx: CanvasRenderingContext2D;
-    
+
     images: object = {};
-    playerWidth: number = 24;
-    playerHeight: number = 32;
-    playerMultiplier: number = 3;
-    
+
     constructor (socket: SocketIOClient.Socket, ctx: CanvasRenderingContext2D)
     {
         this.socket = socket;
-        
+
         this.ctx = ctx;
-        
+
         var avatars = [
             "M",
             "F",
@@ -27,57 +26,51 @@ export class World
             this.images[avatar] = image;
         }
     }
-    
-    drawPlayer (x: number, y: number, avatar: string, facing: string, frame: number)
+
+    drawObject (x: number, y: number, avatar: string, facing: string, frame: number)
     {
-        var width = this.playerWidth;
-        var height = this.playerHeight;
-        var multiplier = this.playerMultiplier;
-        
+        var width = 24;
+        var height = 32;
+
+        var innerX = width * frame;
         switch (facing) {
             case "up":
-                var imageY = 0;
+                var innerY = 0;
                 break;
-    
+
             case "right":
-                var imageY = height;
+                var innerY = height;
                 break;
-    
+
             case "down":
-                var imageY = height * 2;
+                var innerY = height * 2;
                 break;
-    
+
             case "left":
-                var imageY = height * 3;
+                var innerY = height * 3;
                 break;
         }
-    
-        this.ctx.drawImage(
-            this.images[avatar],
-            width * frame,
-            imageY,
-            width,
-            height,
-            x,
-            y,
-            width * multiplier,
-            height * multiplier,
-        );
+
+        var object = new WorldObject(this.ctx, this.images[avatar], width, height);
+        object.scaleBy(3);
+        object.draw(x, y, innerX, innerY);
+
+        return object;
     }
-    
+
     drawMessage (x: number, y: number, message: string)
     {
         this.ctx.fillStyle = "#000000";
-        
+
         this.ctx.fillText(message, x, y);
     }
-    
+
     drawHealth (x: number, y: number, hp: number, width: number = 50, height: number = 5)
     {
         // Create the border for the health bar
         this.ctx.fillStyle = "#000000";
         this.ctx.fillRect(x - 1, y - 1, width + 2, height + 2);
-        
+
         // Create the health bar
         this.ctx.fillStyle = "#FF0000";
         this.ctx.fillRect(x, y, ((hp / 100) * width), height);
