25bc24bd328b6fe3c5b060724d907196b9b3ed16
tomcphr <tomcphr@pm.me>
2018-08-03 10:23:13 +0100

    Better maps system

--- host/src/Tiles.js
+++ host/src/Tiles.js
@@ -1,15 +1,24 @@
 class Tiles {
     constructor () {
         this.width = 32;
+
         this.height = 32;
+
+        this.maps = {
+            "overworld" :   require("../maps/overworld.json")
+        };
     };
 
     getWidth () {
         return this.width;
-    }
+    };
 
     getHeight () {
         return this.height;
-    }
+    };
+
+    getMap (type) {
+        return this.maps[type];
+    };
 }
 module.exports = Tiles;
